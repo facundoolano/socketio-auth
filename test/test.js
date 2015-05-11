38816406e83d2e8c898e2d150d80059211d23c69
Martin Paulucci <martin.c.paulucci@gmail.com>
2015-05-11 18:48:46 -0300

    Add jshint/jscs and fix styles.

--- test/test.js
+++ test/test.js
@@ -1,135 +1,145 @@
+'use strict';
+
 var assert = require('assert');
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 
 function NamespaceMock(name) {
-	this.name = name;
-	this.sockets = [];
-	this.connected = {}
+  this.name = name;
+  this.sockets = [];
+  this.connected = {};
 }
+
 util.inherits(NamespaceMock, EventEmitter);
 
 NamespaceMock.prototype.connect = function(client) {
-	this.sockets.push(client);
-	this.connected[client.id] = client;
-	this.emit('connection', client);
-}
-
+  this.sockets.push(client);
+  this.connected[client.id] = client;
+  this.emit('connection', client);
+};
 
 function ServerSocketMock () {
-	this.nsps = {
-    "/User": new NamespaceMock("/User"),
-    "/Message": new NamespaceMock("/Message")
+  this.nsps = {
+    '/User': new NamespaceMock('/User'),
+    '/Message': new NamespaceMock('/Message')
   };
 }
+
 util.inherits(ServerSocketMock, EventEmitter);
 
 ServerSocketMock.prototype.connect = function(nsp, client) {
-	this.emit('connection', client);
-	this.nsps[nsp].connect(client);
-}
-        
+  this.emit('connection', client);
+  this.nsps[nsp].connect(client);
+};
 
 function ClientSocketMock(id) {
-	this.id = id;
-	this.client = {}
+  this.id = id;
+  this.client = {};
 }
 util.inherits(ClientSocketMock, EventEmitter);
-        
+
 ClientSocketMock.prototype.disconnect = function() {
-	this.emit('disconnect');
-}
+  this.emit('disconnect');
+};
 
 function authenticate(data, cb) {
-	if(!data.token) return cb(new Error("Missing credentials"));
-	cb(null, data.token == "fixedtoken");
+  if (!data.token) {
+    cb(new Error('Missing credentials'));
+  }
+
+  cb(null, data.token === 'fixedtoken');
 }
 
-describe('Server socket authentication', function(){
-	var server;
-	var client;
+describe('Server socket authentication', function() {
+  var server;
+  var client;
 
-	beforeEach(function(){
+  beforeEach(function() {
     server = new ServerSocketMock();
+
     require('../lib/socketio-auth')(server, {
       timeout:80,
       authenticate: authenticate
-      });
-    client = new ClientSocketMock(5);
-	});
+    });
 
-	it('Should mark the socket as unauthenticated upon connection', function(done) {
-    assert(client.auth == undefined);
-    server.connect("/User", client);
-    process.nextTick(function(){
-      assert(client.auth == false);
+    client = new ClientSocketMock(5);
+  });
+
+  it('Should mark the socket as unauthenticated upon connection', function(done) {
+    assert(client.auth === undefined);
+    server.connect('/User', client);
+    process.nextTick(function() {
+      assert(client.auth === false);
       done();
     });
-	});
+  });
 
   it('Should not send messages to unauthenticated sockets', function(done) {
-    server.connect("/User", client);
-    process.nextTick(function(){
+    server.connect('/User', client);
+    process.nextTick(function() {
       assert(!server.nsps['/User'][5]);
       done();
     });
-	});
+  });
 
   it('Should disconnect sockets that do not authenticate', function(done) {
-    server.connect("/User", client);
-    client.on('disconnect', function(){
-    	done();
+    server.connect('/User', client);
+    client.on('disconnect', function() {
+      done();
     });
-	});
+  });
 
   it('Should authenticate with valid credentials', function(done) {
-    server.connect("/User", client);
-    process.nextTick(function(){
-      client.on('authenticated', function(){
+    server.connect('/User', client);
+    process.nextTick(function() {
+      client.on('authenticated', function() {
         assert(client.auth);
         done();
       });
-      client.emit('authentication', {token: "fixedtoken"});
+      client.emit('authentication', {token: 'fixedtoken'});
     });
-	});
+  });
 
   it('Should call post auth function', function(done) {
     server = new ServerSocketMock();
     client = new ClientSocketMock(5);
 
     var postAuth = function(socket, tokenData) {
-      assert.equal(tokenData.token, "fixedtoken");
+      assert.equal(tokenData.token, 'fixedtoken');
       assert.equal(socket, client);
       done();
-    }
-    
+    };
+
     require('../lib/socketio-auth')(server, {
       timeout:80,
       authenticate: authenticate,
       postAuthenticate: postAuth
     });
 
-    server.connect("/User", client);
-    process.nextTick(function(){
-      client.emit('authentication', {token: "fixedtoken"});
+    server.connect('/User', client);
+
+    process.nextTick(function() {
+      client.emit('authentication', {token: 'fixedtoken'});
     });
-	});
+  });
 
   it('Should send updates to authenticated sockets', function(done) {
-    server.connect("/User", client);
-    process.nextTick(function(){
-      client.on('authenticated', function(){
+    server.connect('/User', client);
+
+    process.nextTick(function() {
+      client.on('authenticated', function() {
         assert.equal(server.nsps['/User'].connected[5], client);
         done();
       });
-      client.emit('authentication', {token: "fixedtoken"});
+      client.emit('authentication', {token: 'fixedtoken'});
     });
   });
 
   it('Should not authenticate without credentials', function(done) {
-    server.connect("/User", client);
-    process.nextTick(function(){
-      client.once('disconnect', function(){
+    server.connect('/User', client);
+
+    process.nextTick(function() {
+      client.once('disconnect', function() {
         done();
       });
       client.emit('authentication', {});
@@ -137,13 +147,13 @@ describe('Server socket authentication', function(){
   });
 
   it('Should not authenticate with invalid credentials', function(done) {
-    server.connect("/User", client);
-    process.nextTick(function(){
-      client.once('disconnect', function(){
+    server.connect('/User', client);
+
+    process.nextTick(function() {
+      client.once('disconnect', function() {
         done();
       });
-      client.emit('authentication', {token: "invalid"});
+      client.emit('authentication', {token: 'invalid'});
     });
   });
-
 });
